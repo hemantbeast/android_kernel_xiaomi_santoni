b7c4ea07301dd1b0c090a06968b2368a12c5200d
Sultan Alsawaf <sultan@kerneltoast.com>
2017-12-25 20:20:08 -0800

    qpnp-fg: Don't bail out when debugfs creation fails
    Debugfs creation failure is a non-fatal error. Don't kill the driver when
    this happens.
    
    Skip debugfs creation entirely when CONFIG_DEBUG_FS is disabled.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

--- drivers/power/qpnp-fg.c
+++ drivers/power/qpnp-fg.c
@@ -9041,6 +9041,7 @@ static int fg_probe(struct spmi_device *spmi)
 	 */
 	chip->batt_psy_name = "battery";
 
+#ifdef CONFIG_DEBUG_FS
 	if (chip->mem_base) {
 		rc = fg_dfs_create(chip);
 		if (rc < 0) {
@@ -9048,6 +9049,7 @@ static int fg_probe(struct spmi_device *spmi)
 			goto power_supply_unregister;
 		}
 	}
+#endif
 
 	/* Fake temperature till the actual temperature is read */
 	chip->last_good_temp = 250;
@@ -9066,8 +9068,10 @@ static int fg_probe(struct spmi_device *spmi)
 
 	return rc;
 
+#ifdef CONFIG_DEBUG_FS
 power_supply_unregister:
 	power_supply_unregister(&chip->bms_psy);
+#endif
 cancel_work:
 	fg_cancel_all_works(chip);
 of_init_fail:
