LineageOS/android_kernel_oneplus_msm8996

mdss: livedisplay: Add support for DCI-P3 mode
Change-Id: I9a5c0101946e1edd06aebf9a247b2969a0e019bf

drivers/video/msm/mdss/mdss_livedisplay.c
@@ -164,6 +164,9 @@ int mdss_livedisplay_update(struct mdss_dsi_ctrl_pdata *ctrl_pdata,
 	if ((mlc->caps & MODE_SRGB) && (types & MODE_SRGB))
 		len += mlc->srgb_enabled ? mlc->srgb_on_cmds_len : mlc->srgb_off_cmds_len;
 
+	if ((mlc->caps & MODE_DCI_P3) && (types & MODE_DCI_P3))
+		len += mlc->dci_p3_enabled ? mlc->dci_p3_on_cmds_len : mlc->dci_p3_off_cmds_len;
+
 	if (is_cabc_cmd(types) && is_cabc_cmd(mlc->caps)) {
 
 		// The CABC command on most modern panels is also responsible for
@@ -239,6 +242,17 @@ int mdss_livedisplay_update(struct mdss_dsi_ctrl_pdata *ctrl_pdata,
 		} else {
 			memcpy(cmd_buf + dlen, mlc->srgb_off_cmds, mlc->srgb_off_cmds_len);
 			dlen += mlc->srgb_off_cmds_len;
+		}
+	}
+
+	// DCI-P3 mode
+	if ((mlc->caps & MODE_DCI_P3) && (types & MODE_DCI_P3)) {
+		if (mlc->dci_p3_enabled) {
+			memcpy(cmd_buf + dlen, mlc->dci_p3_on_cmds, mlc->dci_p3_on_cmds_len);
+			dlen += mlc->dci_p3_on_cmds_len;
+		} else {
+			memcpy(cmd_buf + dlen, mlc->dci_p3_off_cmds, mlc->dci_p3_off_cmds_len);
+			dlen += mlc->dci_p3_off_cmds_len;
 		}
 	}
 
@@ -408,6 +422,34 @@ static ssize_t mdss_livedisplay_set_srgb(struct device *dev,
 	return count;
 }
 
+static ssize_t mdss_livedisplay_get_dci_p3(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	struct fb_info *fbi = dev_get_drvdata(dev);
+	struct msm_fb_data_type *mfd = (struct msm_fb_data_type *)fbi->par;
+	struct mdss_livedisplay_ctx *mlc = get_ctx(mfd);
+
+	return sprintf(buf, "%d\n", mlc->dci_p3_enabled);
+}
+
+static ssize_t mdss_livedisplay_set_dci_p3(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t count)
+{
+	int value = 0;
+	struct fb_info *fbi = dev_get_drvdata(dev);
+	struct msm_fb_data_type *mfd = (struct msm_fb_data_type *)fbi->par;
+	struct mdss_livedisplay_ctx *mlc = get_ctx(mfd);
+
+	sscanf(buf, "%du", &value);
+	if ((value == 0 || value == 1)
+			&& value != mlc->dci_p3_enabled) {
+		mlc->dci_p3_enabled = value;
+		mdss_livedisplay_event(mfd, MODE_DCI_P3);
+	}
+
+	return count;
+}
+
 static ssize_t mdss_livedisplay_get_color_enhance(struct device *dev,
 		struct device_attribute *attr, char *buf)
 {
@@ -513,6 +555,7 @@ static DEVICE_ATTR(preset, S_IRUGO | S_IWUSR | S_IWGRP, mdss_livedisplay_get_pre
 static DEVICE_ATTR(num_presets, S_IRUGO, mdss_livedisplay_get_num_presets, NULL);
 static DEVICE_ATTR(hbm, S_IRUGO | S_IWUSR | S_IWGRP, mdss_livedisplay_get_hbm, mdss_livedisplay_set_hbm);
 static DEVICE_ATTR(srgb, S_IRUGO | S_IWUSR | S_IWGRP, mdss_livedisplay_get_srgb, mdss_livedisplay_set_srgb);
+static DEVICE_ATTR(dci_p3, S_IRUGO | S_IWUSR | S_IWGRP, mdss_livedisplay_get_dci_p3, mdss_livedisplay_set_dci_p3);
 
 int mdss_livedisplay_parse_dt(struct device_node *np, struct mdss_panel_info *pinfo)
 {
@@ -579,6 +622,15 @@ int mdss_livedisplay_parse_dt(struct device_node *np, struct mdss_panel_info *pi
 				"cm,mdss-livedisplay-srgb-off-cmd", &mlc->srgb_off_cmds_len);
 		if (mlc->srgb_off_cmds_len)
 			mlc->caps |= MODE_SRGB;
+	}
+
+	mlc->dci_p3_on_cmds = of_get_property(np,
+			"cm,mdss-livedisplay-dci-p3-on-command", &mlc->dci_p3_on_cmds_len);
+	if (mlc->dci_p3_on_cmds_len) {
+		mlc->dci_p3_off_cmds = of_get_property(np,
+			"cm,mdss-livedisplay-dci-p3-off-command", &mlc->dci_p3_off_cmds_len);
+		if (mlc->dci_p3_off_cmds_len)
+			mlc->caps |= MODE_DCI_P3;
 	}
 
 	mlc->ce_on_cmds = of_get_property(np,
@@ -649,6 +701,12 @@ int mdss_livedisplay_create_sysfs(struct msm_fb_data_type *mfd)
 
 	if (mlc->caps & MODE_SRGB) {
 		rc = sysfs_create_file(&mfd->fbi->dev->kobj, &dev_attr_srgb.attr);
+		if (rc)
+			goto sysfs_err;
+	}
+
+	if (mlc->caps & MODE_DCI_P3) {
+		rc = sysfs_create_file(&mfd->fbi->dev->kobj, &dev_attr_dci_p3.attr);
 		if (rc)
 			goto sysfs_err;
 	}

hemantbeast/android_kernel_xiaomi_santoni

drivers/video/msm/mdss/mdss_livedisplay.c
@@ -164,6 +164,9 @@ int mdss_livedisplay_update(struct mdss_dsi_ctrl_pdata *ctrl_pdata,
 	if ((mlc->caps & MODE_SRGB) && (types & MODE_SRGB))
 		len += mlc->srgb_enabled ? mlc->srgb_on_cmds_len : mlc->srgb_off_cmds_len;
 
+	if ((mlc->caps & MODE_DCI_P3) && (types & MODE_DCI_P3))
+		len += mlc->dci_p3_enabled ? mlc->dci_p3_on_cmds_len : mlc->dci_p3_off_cmds_len;
+
 	if (is_cabc_cmd(types) && is_cabc_cmd(mlc->caps)) {
 
 		// The CABC command on most modern panels is also responsible for
@@ -239,6 +242,17 @@ int mdss_livedisplay_update(struct mdss_dsi_ctrl_pdata *ctrl_pdata,
 		} else {
 			memcpy(cmd_buf + dlen, mlc->srgb_off_cmds, mlc->srgb_off_cmds_len);
 			dlen += mlc->srgb_off_cmds_len;
+		}
+	}
+
+	// DCI-P3 mode
+	if ((mlc->caps & MODE_DCI_P3) && (types & MODE_DCI_P3)) {
+		if (mlc->dci_p3_enabled) {
+			memcpy(cmd_buf + dlen, mlc->dci_p3_on_cmds, mlc->dci_p3_on_cmds_len);
+			dlen += mlc->dci_p3_on_cmds_len;
+		} else {
+			memcpy(cmd_buf + dlen, mlc->dci_p3_off_cmds, mlc->dci_p3_off_cmds_len);
+			dlen += mlc->dci_p3_off_cmds_len;
 		}
 	}
 
@@ -407,6 +421,34 @@ static ssize_t mdss_livedisplay_set_srgb(struct device *dev,
 	return count;
 }
 
+static ssize_t mdss_livedisplay_get_dci_p3(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	struct fb_info *fbi = dev_get_drvdata(dev);
+	struct msm_fb_data_type *mfd = (struct msm_fb_data_type *)fbi->par;
+	struct mdss_livedisplay_ctx *mlc = get_ctx(mfd);
+
+	return sprintf(buf, "%d\n", mlc->dci_p3_enabled);
+}
+
+static ssize_t mdss_livedisplay_set_dci_p3(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t count)
+{
+	int value = 0;
+	struct fb_info *fbi = dev_get_drvdata(dev);
+	struct msm_fb_data_type *mfd = (struct msm_fb_data_type *)fbi->par;
+	struct mdss_livedisplay_ctx *mlc = get_ctx(mfd);
+
+	sscanf(buf, "%du", &value);
+	if ((value == 0 || value == 1)
+			&& value != mlc->dci_p3_enabled) {
+		mlc->dci_p3_enabled = value;
+		mdss_livedisplay_event(mfd, MODE_DCI_P3);
+	}
+
+	return count;
+}
+
 static ssize_t mdss_livedisplay_get_color_enhance(struct device *dev,
 		struct device_attribute *attr, char *buf)
 {
@@ -512,6 +554,7 @@ static DEVICE_ATTR(preset, S_IRUGO | S_IWUSR | S_IWGRP, mdss_livedisplay_get_pre
 static DEVICE_ATTR(num_presets, S_IRUGO, mdss_livedisplay_get_num_presets, NULL);
 static DEVICE_ATTR(hbm, S_IRUGO | S_IWUSR | S_IWGRP, mdss_livedisplay_get_hbm, mdss_livedisplay_set_hbm);
 static DEVICE_ATTR(srgb, S_IRUGO | S_IWUSR | S_IWGRP, mdss_livedisplay_get_srgb, mdss_livedisplay_set_srgb);
+static DEVICE_ATTR(dci_p3, S_IRUGO | S_IWUSR | S_IWGRP, mdss_livedisplay_get_dci_p3, mdss_livedisplay_set_dci_p3);
 
 int mdss_livedisplay_parse_dt(struct device_node *np, struct mdss_panel_info *pinfo)
 {
@@ -578,6 +621,15 @@ int mdss_livedisplay_parse_dt(struct device_node *np, struct mdss_panel_info *pi
 				"cm,mdss-livedisplay-srgb-off-cmd", &mlc->srgb_off_cmds_len);
 		if (mlc->srgb_off_cmds_len)
 			mlc->caps |= MODE_SRGB;
+	}
+
+	mlc->dci_p3_on_cmds = of_get_property(np,
+			"cm,mdss-livedisplay-dci-p3-on-cmd", &mlc->dci_p3_on_cmds_len);
+	if (mlc->dci_p3_on_cmds_len) {
+		mlc->dci_p3_off_cmds = of_get_property(np,
+			"cm,mdss-livedisplay-dci-p3-off-cmd", &mlc->dci_p3_off_cmds_len);
+		if (mlc->dci_p3_off_cmds_len)
+			mlc->caps |= MODE_DCI_P3;
 	}
 
 	mlc->ce_on_cmds = of_get_property(np,
@@ -648,6 +700,12 @@ int mdss_livedisplay_create_sysfs(struct msm_fb_data_type *mfd)
 
 	if (mlc->caps & MODE_SRGB) {
 		rc = sysfs_create_file(&mfd->fbi->dev->kobj, &dev_attr_srgb.attr);
+		if (rc)
+			goto sysfs_err;
+	}
+
+	if (mlc->caps & MODE_DCI_P3) {
+		rc = sysfs_create_file(&mfd->fbi->dev->kobj, &dev_attr_dci_p3.attr);
 		if (rc)
 			goto sysfs_err;
 	}